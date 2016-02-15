73a6b71b120fd3fa894c5353fbd25f78b198ed71
sherigr <SGrossman00@gmail.com>
2016-02-15 15:52:40 -0500

    initial popover

--- js/script.js
+++ js/script.js
@@ -18,13 +18,67 @@ $(function() {
 /*==========================================
  GOOGLE MAP
 =====================================================*/
-    $('#map').click(function () {
-        $('#map iframe').css("pointer-events", "auto");
+    // $('#map').click(function () {
+    //     $('#map iframe').css("pointer-events", "auto");
+    // });
+    
+    // $( "#map").mouseleave(function() {
+    //   $('#map iframe').css("pointer-events", "none"); 
+    // });
+
+    // Added Second Map
+        $('#map, #second-map').click(function () {
+        $('#map iframe, #second-map iframe').css("pointer-events", "auto");
     });
     
-    $( "#map" ).mouseleave(function() {
-      $('#map iframe').css("pointer-events", "none"); 
+    $( "#map, #second-map" ).mouseleave(function() {
+      $('#map iframe, #second-map iframe').css("pointer-events", "none"); 
     });
+
+/*==========================================
+ TREATMENT
+=====================================================*/
+/* hide treatment until hover*/
+// $('p.treatment').hide();
+// $('.treated').hover(function(e) {
+// 	e.preventDefault();
+// 	$(this).siblings('.treatment').fadeIn(500);
+// 	$(this).siblings('.treatment').fadeOut(1000);
+	// $(this).next().fadeIn(500);
+	// $(this).next().fadeOut(1000);
+// });
+
+//This Works
+// $('.treatment-type').hide();
+// $('.img-circle').mouseover(function() {
+// 	$(this).siblings('.treatment-type').show().addClass('');
+// });
+// $('.img-circle').mouseout(function() {
+// 	$(this).siblings('.treatment-type').hide();
+// });
+
+//USING POPOVER
+// this works speicifc to foot
+// $('#foot').mouseover(function() {
+// 	$('#foot').popover('show');
+// });
+// $('#foot').mouseout(function() {
+// 	$('#foot').popover('hide');
+// });
+
+$('#treatments img').mouseover(function() {
+	$(this).popover('show');
+});
+$('#treatments img').mouseout(function() {
+	$(this).popover('hide');
+});
+
+/*==========================================
+ PARALLAX SECTION - paradigm def
+=====================================================*/
+$('.parallax-window').parallax({imageSrc: 'img/ShadowPerson.jpg'});
+
+
 
 /*==========================================
   FOOTER
