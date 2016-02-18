2117ab678c1f80f5ebbdb4c69450cc044ccaa87f
sherigr <SGrossman00@gmail.com>
2016-02-18 12:14:31 -0500

    swap images

--- js/script.js
+++ js/script.js
@@ -2,17 +2,35 @@
 $(function() {
 
 /*==========================================
+NAVBAR
+=====================================================*/
+//Enables Navbar dropdown menu on hover, can click open on mobile
+//NO SCROLLING DOWN FOR FULL NAVBAR LIST
+// jQuery('ul.nav li.dropdown').hover(function() {
+//   jQuery(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn();
+// }, function() {
+//   jQuery(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut();
+// });
+
+//NO SCROLLING DOWN FOR FULL NAVBAR LIST
+// jQuery('ul.nav li.dropdown').hover(function() {
+//   jQuery(this).find('.dropdown-menu').stop(true, true).show();
+//   jQuery(this).addClass('open');
+// }, function() {
+//   jQuery(this).find('.dropdown-menu').stop(true, true).hide();
+//   jQuery(this).removeClass('open');
+// });
+
+
+/*==========================================
  CAROUSEL SPEED
 =====================================================*/
 	$('#myCarousel').carousel({
-		interval: 3000,
+		interval: 3500,
 		pause: false
 	});
 
-	$('#testimonialCarousel').carousel({
-		interval: 10000,
-		pause: false
-	});
+	
 
 
 /*==========================================
@@ -38,34 +56,8 @@ $(function() {
 /*==========================================
  TREATMENT
 =====================================================*/
-/* hide treatment until hover*/
-// $('p.treatment').hide();
-// $('.treated').hover(function(e) {
-// 	e.preventDefault();
-// 	$(this).siblings('.treatment').fadeIn(500);
-// 	$(this).siblings('.treatment').fadeOut(1000);
-	// $(this).next().fadeIn(500);
-	// $(this).next().fadeOut(1000);
-// });
 
-//This Works
-// $('.treatment-type').hide();
-// $('.img-circle').mouseover(function() {
-// 	$(this).siblings('.treatment-type').show().addClass('');
-// });
-// $('.img-circle').mouseout(function() {
-// 	$(this).siblings('.treatment-type').hide();
-// });
-
-//USING POPOVER
-// this works speicifc to foot
-// $('#foot').mouseover(function() {
-// 	$('#foot').popover('show');
-// });
-// $('#foot').mouseout(function() {
-// 	$('#foot').popover('hide');
-// });
-
+/* Show Diagnoses Description on Hover */
 $('#treatments img').mouseover(function() {
 	$(this).popover('show');
 });
@@ -73,11 +65,13 @@ $('#treatments img').mouseout(function() {
 	$(this).popover('hide');
 });
 
+
+
 /*==========================================
  PARALLAX SECTION - paradigm def
 =====================================================*/
-$('.parallax-window').parallax({imageSrc: 'img/ShadowPerson.jpg'});
-
+// $('.parallax-window').parallax({imageSrc: 'img/ShadowPerson.jpg'});
+$('.parallax-window').parallax({imageSrc: 'img/swimTest.jpg'});
 
 
 /*==========================================
